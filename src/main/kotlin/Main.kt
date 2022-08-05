d44d77dcd57243066aa78920fbf6f9cce0b95419
nadinnah <nadinahmedmohamed@gmail.com>
2022-08-05 13:10:18 +0300

    version 1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+import java.util.Scanner
+fun main() {
+   val number1: Int= Scanner(System.`in`).nextInt()
+
+   val number2: Int= Scanner(System.`in`).nextInt()
+    val result= number1 + number2
+    println("the sum of the 2 numbers is $result")
+
+}
